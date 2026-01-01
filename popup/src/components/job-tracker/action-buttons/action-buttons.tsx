b2735743a996c68c153dea65582f5d6e117e2b13
Shehab Elhariry <cheha6@gmail.com>
2026-01-01 22:52:37 +0100

    fix: removed unecessary code

--- popup/src/components/job-tracker/action-buttons/action-buttons.tsx
+++ popup/src/components/job-tracker/action-buttons/action-buttons.tsx
@@ -1,12 +1,8 @@
-import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.mjs";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.mjs?url";
 import UploadCVButton from "./upload-cv-button";
 import { CvType } from "../../../baseCV";
 import CreateTailoredCVButton from "./create-tailored-cv-button";
 import { JobData } from "../../../types";
 import { isDebugMode } from "../../../const";
-
-pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 interface ActionButtonsProps {
   setCvObject: (cv: CvType | undefined) => void;
